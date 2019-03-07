df07046fb3fdde8b369c2916554dfee400a091b8
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-03-07 19:48:46 +0300

    04

--- 04/horses.js
+++ 04/horses.js
@@ -1,24 +1,36 @@
 let scores = [22, 33, 49, 87, 82, 38, 18, 49, 87, 49, 73, 18, 56, 62]
 
-// Вывести номер и результат каждой лошади в консоль
-// Найти и сохранить самый высокий результат в переменную highScore
-
 let highScore = 0
-let i = 0
-while (i < scores.length) {
-  console.log("i = ", i)
-  console.log("Длина массива, scores.length = ", scores.length)
-  console.log("Теккущий элемент массива, scores[i] = ", scores[i])
-  console.log("highScore = ", highScore)
+// let i = 0
+// while (i < scores.length) {
+//   if (scores[i] > highScore) {
+//     highScore = scores[i]
+//   }
+//   i = i + 1
+// }
+for (let i = 0; i < scores.length; i++) {
   if (scores[i] > highScore) {
     highScore = scores[i]
   }
-  i = i + 1
 }
 console.log("highScore = ", highScore)
 
-
-
+// Найти лошадей с лучшим результатом
+// и сохранить в переменную bestScores
+let bestScores = []
+// let j = 0
+// while (j < scores.length) {
+//   if (scores[j] === highScore) {
+//     bestScores.push(j)
+//   }
+//   j += 1
+// }
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] === highScore) {
+    bestScores.push(i)
+  }
+}
+console.log("bestScores = ", bestScores)
 
 
 
