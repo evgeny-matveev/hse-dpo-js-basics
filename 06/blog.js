eef169f9ad354846bfa55402ab6cb90329ce1d68
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-03-14 21:04:36 +0300

    Blog Fin

--- 06/blog.js
+++ 06/blog.js
@@ -1,12 +1,22 @@
 let toggler = document.getElementById('themeToggler')
+
 toggler.onclick = toggleNightMode
+
 function toggleNightMode() {
   document.body.classList.toggle('is-night')
 }
 
 let menuBtn = document.querySelector('.menu-btn')
+
 menuBtn.onclick = toggleMenuVisibility
+
 function toggleMenuVisibility() {
   let nav = document.querySelector('nav')
   nav.classList.toggle('is-active')
+  
+  if (document.body.style.overflow === 'hidden') {
+    document.body.style.overflow = 'visible'
+  } else {
+    document.body.style.overflow = 'hidden'
+  }
 }
