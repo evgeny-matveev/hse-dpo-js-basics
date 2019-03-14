d5d27540f45072c00e09d8d730230a790321e571
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-03-14 20:56:57 +0300

    Добавили перключение меню по клику на бургер

--- 06/blog.js
+++ 06/blog.js
@@ -1,6 +1,12 @@
 let toggler = document.getElementById('themeToggler')
 toggler.onclick = toggleNightMode
-
 function toggleNightMode() {
-  document.body.classList.toggle("is-night")
+  document.body.classList.toggle('is-night')
+}
+
+let menuBtn = document.querySelector('.menu-btn')
+menuBtn.onclick = toggleMenuVisibility
+function toggleMenuVisibility() {
+  let nav = document.querySelector('nav')
+  nav.classList.toggle('is-active')
 }
