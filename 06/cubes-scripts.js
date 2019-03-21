e646c0b05bc2ea98c61ec82c19919507da025b1a
Eugene Matveyev <hello.eugene.matveyev@gmail.com>
2019-03-21 21:31:48 +0300

    End with tasks

--- 06/cubes-scripts.js
+++ 06/cubes-scripts.js
@@ -2,19 +2,27 @@ let $rectOne = document.getElementById('one')
 let $rectTwo = document.getElementById('two')
 let $rectThree = document.getElementById('three')
 
-$rectOne.onclick = onRectClick
-$rectTwo.onclick = onRectClick
-$rectThree.onclick = onRectClick
+$rectOne.onclick = onRectClickOne
+$rectTwo.onclick = onRectClickTwo
+$rectThree.onclick = onRectClickThree
 
-function onRectClick() {
-  this.style.backgroundColor = getRGBColor()
-  this.classList.toggle('is-cube')
-  this.classList.toggle('is-circle')
-  this.classList.toggle('is-animated')
-  // changeBackground(this)
-  // transformToCube(this)
-  // transformToCircle(this)
-  // toggleAnimation(this)
+function onRectClickOne() {
+  // this.style.backgroundColor = getRGBColor()
+  // this.classList.toggle('is-cube')
+  // this.classList.toggle('is-circle')
+  // this.classList.toggle('is-animated')
+  changeBackground(this)
+  toggleAnimation(this)
+}
+function onRectClickTwo() {
+  transformToCube(this)
+  transformToCircle(this)
+}
+function onRectClickThree() {
+  changeBackground(this)
+  transformToCube(this)
+  transformToCircle(this)
+  toggleAnimation(this)
 }
 function getRGBCode() {
   return Math.floor(Math.random() * 255)
@@ -22,15 +30,15 @@ function getRGBCode() {
 function getRGBColor() {
   return `rgb(${getRGBCode()}, ${getRGBCode()}, ${getRGBCode()})`
 }
-// function toggleAnimation($rect) {
-//   $rect.classList.toggle('is-animated')
-// }
-// function transformToCircle($rect) {
-//   $rect.classList.toggle('is-circle')
-// }
-// function transformToCube($rect) {
-//   $rect.classList.toggle('is-cube')
-// }
-// function changeBackground($rect) {
-//   $rect.style.backgroundColor = getRGBColor()
-// }
+function toggleAnimation($rect) {
+  $rect.classList.toggle('is-animated')
+}
+function transformToCircle($rect) {
+  $rect.classList.toggle('is-circle')
+}
+function transformToCube($rect) {
+  $rect.classList.toggle('is-cube')
+}
+function changeBackground($rect) {
+  $rect.style.backgroundColor = getRGBColor()
+}
